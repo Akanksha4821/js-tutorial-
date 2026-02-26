86ed02cb836865d1b566ce6aec495d4dc95edd6d
Akanksha Jha <akankshaj2002@gmail.com>
2026-02-26 11:26:29 +0000

    second commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=144553
+let accountEmail="akanksha.jha@elevate.law"
+var accountpassword="12345"  // we don't use this -> it does not know about scope
+accountCity="jaipur"
+let accountState
+
+//accountId=2   ->not allowed
+accountEmail="aers@gmail.com"
+accountpassword="2233"
+accountCity="bengaluru"
+
+//console.log(accountId);
+console.table([accountId,accountEmail,accountpassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+//alert("hello")
+console.log(3+3)
+
+let name="akanksha"
+let age=2
+let isLoggedIn=false
+//number
+//bigint
+//boolean
+//string
+//null
+//undefined-variable declared but value not assigned
+//symbol- uniquness
+
+
+//object
+
+console.log(typeof null)
+console.log(typeof undefined)
